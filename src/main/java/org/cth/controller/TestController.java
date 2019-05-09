534915b0fc59301eb1b92d87a987c3c579c8691f
cth <863930059@qq.com>
2019-05-09 11:09:22 +0800

    init

---
+++ src/main/java/org/cth/controller/TestController.java
@@ -0,0 +1,14 @@
+package org.cth.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("test")
+public class TestController {
+    @GetMapping("testGet")
+    public String testGet() {
+        return "hello...";
+    }
+}
